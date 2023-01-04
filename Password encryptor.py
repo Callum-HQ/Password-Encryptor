7656b44d30b017fb4c94bac8b0879258477ab9a3
Kairos Tay <80029462+Kairos-T@users.noreply.github.com>
2023-01-04 17:13:45 +0800

    Update Password encryptor.py

--- Password encryptor.py
+++ Password encryptor.py
@@ -1,11 +1,9 @@
 import hashlib
 
 def encrypt_password(password):
-  # Encrypt the password using the SHA-256 algorithm
   encrypted_password = hashlib.sha256(password.encode()).hexdigest()
   return encrypted_password
 
-# Test the function
 password = "mypassword"
 encrypted_password = encrypt_password(password)
 print(encrypted_password)
